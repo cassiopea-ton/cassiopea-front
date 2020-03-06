43e9193a00a23454f7ac489969543d999e0f227d
Rexvel <teteriv22@gmail.com>
2020-03-06 10:48:43 +0200

    Basic setup

---
+++ src/containers/ConsumerInfo/ConsumerInfo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ConsumerInfo.scss';
+
+function ConsumerInfo() {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default ConsumerInfo;
---
+++ src/containers/OracleInfo/OracleInfo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './OracleInfo.scss';
+
+function OracleInfo() {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default OracleInfo;
