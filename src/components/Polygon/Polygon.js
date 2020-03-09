8b640f016ccbaca82314a78233a063505974363a
Rexvel <teteriv22@gmail.com>
2020-03-09 13:23:26 +0200

    Polygon img and font fix

--- src/components/Polygon/Polygon.js
+++ src/components/Polygon/Polygon.js
@@ -7,7 +7,7 @@ const  Polygon = () =>
         <img className="polygon__img" src={polygon} alt="polygon" />
         <div className="polygon__block">
             <h1 className="polygon__article">Cassiopea</h1>
-            <p className="polygon__text"><span>Real-world data for your smart contracts</span><span>Don’t limit your DAPP.</span> </p>
+            <p className="polygon__text"><span></span>Real-world data for your smart contracts.<span></span> Don’t limit your DAPP.</p>
         </div>
     </div>
 
