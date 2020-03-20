beb8571d705cb02a7a4c9f959bde93a9b868abf1
vird <virdvip@gmail.com>
2020-03-20 18:23:20 +0000

    Updates

---
+++ precache-manifest.d01a5a1fb569ebf6abd060ed7e71d2a3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26deacbdfdbf2eb52731024eb176b8e9",
+    "url": "/cassiopea-front/index.html"
+  },
+  {
+    "revision": "9f414626e9155845061a",
+    "url": "/cassiopea-front/static/css/main.f1a1f99a.chunk.css"
+  },
+  {
+    "revision": "0a56f25b30269eae6cbf",
+    "url": "/cassiopea-front/static/js/2.56f79a6c.chunk.js"
+  },
+  {
+    "revision": "3bc01e540a0c258158db1dbf3191097c",
+    "url": "/cassiopea-front/static/js/2.56f79a6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f414626e9155845061a",
+    "url": "/cassiopea-front/static/js/main.f8a62fcb.chunk.js"
+  },
+  {
+    "revision": "900978a34688bfae4e06",
+    "url": "/cassiopea-front/static/js/runtime-main.7cffaa42.js"
+  },
+  {
+    "revision": "e65a7ac9d31de32db82c76c5ab5d4bfd",
+    "url": "/cassiopea-front/static/media/OraclePolygon.e65a7ac9.png"
+  },
+  {
+    "revision": "c61a33ce8275a6232ab470e27e1da588",
+    "url": "/cassiopea-front/static/media/Polygon.c61a33ce.png"
+  },
+  {
+    "revision": "dfe98d7e7d228196debfeecba99c2491",
+    "url": "/cassiopea-front/static/media/header-logo.dfe98d7e.png"
+  },
+  {
+    "revision": "d70cd7e18a4a81150a3cdb41b16ee0a7",
+    "url": "/cassiopea-front/static/media/howToUse.d70cd7e1.png"
+  },
+  {
+    "revision": "d869d96163a68255411020d79e7b6e4c",
+    "url": "/cassiopea-front/static/media/needData.d869d961.png"
+  },
+  {
+    "revision": "d2ecb9b96ca1337086ae24c7009b4104",
+    "url": "/cassiopea-front/static/media/networkAlt.d2ecb9b9.png"
+  },
+  {
+    "revision": "4b5efe664783e1e5ef2edff323be870f",
+    "url": "/cassiopea-front/static/media/oracleInfo.4b5efe66.png"
+  }
+]);
