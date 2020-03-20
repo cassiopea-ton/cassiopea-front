fb2a9c500e1d935cc2a0d45e1042cd5b9dcfdb5f
Rexvel <teteriv22@gmail.com>
2020-03-20 18:27:53 +0200

    BeOracleInfo

--- src/containers/BeOracle/BeOracle.js
+++ src/containers/BeOracle/BeOracle.js
@@ -1,9 +1,12 @@
 import React from "react";
 import "./BeOracle.scss";
+import BeOraclePolygon from "../../components/BeOraclePolygon/BeOraclePolygon";
+import BeOracleInfo from "../../components/BeOracleInfo/BeOracleInfo";
 
-const BeOracle = () => (
+const BeOracle = (props) => (
    <div className="become__oracle__container">
-   
+       <BeOraclePolygon />
+       <BeOracleInfo />
   </div>
-);
+);  
 export default BeOracle;
