8cc55a57848966bf27f7be1c46daa115c67e0242
Rexvel <teteriv22@gmail.com>
2020-04-29 14:33:00 +0300

    Deleted unused stuff

--- src/components/StatTableBody/objectIterator1.js
+++
@@ -1,4 +0,0 @@
-const objectIterator1 = (iteratedObj) => {
-  let arr = Object.values(iteratedObj).map(i => iteratedObj[i]);
-  return arr;
-};
--- src/components/StatTableHead/StatTableHead.jsx
+++ src/components/StatTableHead/StatTableHead.jsx
@@ -1,22 +1,12 @@
-import React from 'react';
+import React from 'react'
+import { tableHeadInfo } from '../StatTableBody/dataStat'
+import './StatTableHead.scss'
 
-import './StatTableHead.scss';
-
-const tableInfo = [
-  'Address',
-  'Type',
-  'Min. Oracles ',
-  'Oracle Counter',
-  'Last Update',
-  'Frequency',
-  'Tag',
-  'Source',
-];
 
 export default () => (
   <thead className="table__head">
     <tr>
-      {tableInfo.map((item, index) => (
+      {tableHeadInfo.map((item, index) => (
         <th key={index.id} data={item} className="table__article">
           {item}
         </th>
